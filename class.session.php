2b486ef5909f09620257ce031b9e445ea763878c
daelmo <justallphine@googlemail.com>
2014-12-07 20:08:24 +0100

    added session class

---
+++ class.session.php
@@ -0,0 +1,17 @@
+<?php
+class session{
+    //start session
+    public function start(){
+        
+    }
+    
+    //end session
+    public function end(){
+        
+    }
+    
+    
+}
+
+
+?>
