6dcf82e4d6ea6f5f3e2ca1ef60eeea8faed6995b
Josephine Rehak <justallphine@googlemail.com>
2014-12-29 00:02:11 +0100

    added slider

--- php_class/class.mainsite.php
+++ php_class/class.mainsite.php
@@ -1,5 +1,20 @@
 <?php
 class mainsite extends site{
+
+	private $slider;
 	
+	public function __construct($ID) {
+		parent::__construct($ID);
+		$this->slider = new slider();
+	}
+	public function getSlider() {
+		return $this->slider;
+	}
+
+	public function setSlider($slider) {
+		$this->slider = $slider;
+	}
+
+
 }
 ?>
