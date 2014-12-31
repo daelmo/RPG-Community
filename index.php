fd603bf1583579a9813af35a7887a56e49d690c5
Josephine Rehak <justallphine@googlemail.com>
2014-12-31 15:15:56 +0100

    included new code

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php 
 	require_once './php_inc/inc.header.php';
-	//CREATE FILE
-require_once 'php_inc/inc.htmlhead.php';
-require_once 'php_inc/inc.htmlmenu.php';
-
+//CREATE FILE
 	//create site
 	$site = new mainsite(1);
-
 	//create Session
-	$site->getSession()->startSession();
+	
+//CREATE PAGE	
+	require_once 'php_inc/inc.htmlhead.php';
+	require_once 'php_inc/inc.htmlmenu.php';
+	//build Slider
 	$site->getSlider()->build();
 ?> 
 
-<!--- content --->
 
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" ></script>  
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.5.3/jquery-ui.min.js" ></script> 
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" ></script>  
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.5.3/jquery-ui.min.js" ></script> 
 		
 <?php
 require_once './php_inc/inc.footer.php';
